51c1b297be2f913df2e9a295c532c3221f693246
alkoasma <alkoasma@mcmaster.ca>
2021-03-27 23:10:08 -0400

    Added Calendar

--- HealthyHabits/App.js
+++ HealthyHabits/App.js
@@ -1,33 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import Swiper from 'react-native-swiper/src'
-import Calendar from './src/components/Calendar'
-import MountainView from './src/components/MountainView'
-import GameDisplay from './src/components/GameDisplay'
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import Swiper from "react-native-swiper/src";
+import CalendarPage from "./src/components/Calendar";
+import MountainView from "./src/components/MountainView";
+import GameDisplay from "./src/components/GameDisplay";
+import { StyleSheet, Text, View } from "react-native";
 
 const App = () => {
   return (
-    <Swiper
-      loop={false}
-      showsPagination={false}
-      index={1}
-    >
-      
-      <Calendar styles={styles}/>
-      <MountainView styles={styles}/>
-      <GameDisplay styles={styles}/>   
+    <Swiper loop={false} showsPagination={false} index={1}>
+      <CalendarPage styles={styles} />
+      <MountainView styles={styles} />
+      <GameDisplay styles={styles} />
     </Swiper>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
 
-export default App
+export default App;
--- HealthyHabits/src/components/Calendar.jsx
+++ HealthyHabits/src/components/Calendar.jsx
@@ -1,12 +1,60 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Calendar, CalendarList, Agenda } from "react-native-calendars";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Header } from "react-native-elements";
+import { Dayjs } from "dayjs";
 
-const Calendar = ({styles}) => {
-    return (
-        <View style={styles.container}>
-            <Text>I AM FROM CALENDAR</Text>
-        </View>
-    )
+function join(t, a, s) {
+  function format(m) {
+    let f = new Intl.DateTimeFormat("en", m);
+    return f.format(t);
+  }
+  return a.map(format).join(s);
 }
 
-export default Calendar
+var a = [{ year: "numeric" }, { month: "2-digit" }, { day: "2-digit" }];
+var sToday = join(new Date(), a, "-");
+
+const CalendarPage = ({ styles }) => {
+  return (
+    <SafeAreaProvider>
+      <Header
+        statusBarProps={{ barStyle: "light-content" }}
+        barStyle="light-content" // or directly
+        rightComponent={{ icon: "settings", color: "#fff" }}
+        centerComponent={{
+          text: "Calendar",
+          style: { color: "#FFF", fontSize: 22 }
+        }}
+        containerStyle={{
+          backgroundColor: "#85C3CF",
+          justifyContent: "space-around"
+        }}
+      />
+      <View style={styles.Calendar}>
+        <CalendarList
+          // Callback which gets executed when visible months change in scroll view. Default = undefined
+          onVisibleMonthsChange={months => {
+            console.log("now these months are visible", months);
+          }}
+          // Max amount of months allowed to scroll to the past. Default = 50
+          pastScrollRange={50}
+          // Max amount of months allowed to scroll to the future. Default = 50
+          futureScrollRange={50}
+          // Enable or disable scrolling of calendar list
+          scrollEnabled={true}
+          // Enable or disable vertical scroll indicator. Default = false
+          showScrollIndicator={true}
+          onDayPress={day => {
+            console.log("selected day", day);
+          }}
+          maxDate={new Date()}
+          vertical={true}
+        />
+      </View>
+    </SafeAreaProvider>
+  );
+};
+
+export default CalendarPage;
