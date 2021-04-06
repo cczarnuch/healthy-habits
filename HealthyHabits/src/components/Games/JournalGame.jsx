dcf209c3fe6f30eac1c766904cb7cbcc8496b1b1
Michelle Domagala-Tang <45515385+domagalm@users.noreply.github.com>
2021-04-06 13:01:07 -0400

    Updated journal game

--- HealthyHabits/src/components/Games/JournalGame.jsx
+++ HealthyHabits/src/components/Games/JournalGame.jsx
@@ -1,32 +1,62 @@
 import React from "react";
-import {View, Text, Icon} from "react-native";
-import { Header } from "react-native-elements";
+import {View, Text, Icon, TextInput, Button} from "react-native";
+import { Header, Slider } from "react-native-elements";
+import { BackgroundImage } from "react-native-elements/dist/config";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import styles from '../../styles/main.js';
-//import LinearGradient from 'react-native-linear-gradient';
+import journalBackground from '../../assets/gradient.jpg';
 
 function displayMountainScreen() {
     // TODO: navigation
     console.log("return to mountain pressed")
 }
 
+function submitEntry(){
+    console.log("submit entry button pressed")
+}
+
 const JournalGame = () => {
+    const [text, onChangeText] = React.useState(null);
+    const [sliderVal, setSliderValue] = React.useState(null);
+
     return (
+        <SafeAreaProvider>
+        <BackgroundImage source={journalBackground} style={styles.img}>
         <View>           
             <Header
                 statusBarProps={{ barStyle: "light-content" }}
                 barStyle="light-content" // or directly
-                //rightComponent={<Icon name="settings" color="white" onPress={() => displayMountainScreen()}></Icon> }
+                rightComponent={{}}
                 centerComponent={{
-                text: "Your Progression",
+                text: "Journal Entry",
                 style: { color: "#FFF", fontSize: 22 }
                 }}
                 containerStyle={{
-                backgroundColor: "transparent",
+                backgroundColor: "#85C3CF",
                 justifyContent: "space-around"
                 }}
             />
-            <Text>This is the Journal Screen</Text>
+            <Text>Welcome to Journal Entry. This minigame is meant to help you reflect on your day, the good and bad.</Text>
+            
+            <TextInput 
+        style={styles.input} value={text} onChangeText={onChangeText} placeholder="Enter your thoughts about the day"></TextInput>
+            
+            <View>
+                <Slider style={styles.slider}
+                        minimumTrackTintColor="#85C3CF"
+                        onSlidingComplete={(sliderVal) => setSliderValue(sliderVal)}
+                        minimumValue={0}
+                        maximumValue={10}
+                        step={1}
+                        value={sliderVal}
+                ></Slider>
+                <Text>{sliderVal}</Text>
+            </View>
+            
+            <Button title="Submit" onPress={submitEntry}/>
        </View>
+       </BackgroundImage>
+       </SafeAreaProvider>       
     )
 }
 
