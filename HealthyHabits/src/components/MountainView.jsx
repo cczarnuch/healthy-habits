5ab1c008502f97e9bc5337c3a2b95ff11ac8f233
Michelle Domagala-Tang <45515385+domagalm@users.noreply.github.com>
2021-04-06 14:08:48 -0400

    Added navigation

--- HealthyHabits/src/components/MountainView.jsx
+++ HealthyHabits/src/components/MountainView.jsx
@@ -6,20 +6,24 @@ import manPicture from '../assets/hike.png';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Header, Icon } from "react-native-elements";
 import { ImageBackground } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 function displaySettingsScreen() {
     // TODO: add navigation between pages
     console.log("settings button pressed")
+  //  navigation.navigate('WorkoutGame')
 }
 
-const MountainView = () => {
+const MountainView = ({ navigation }) => {
     return (
         <SafeAreaProvider>
             <ImageBackground source={mPicture} style={styles.img}>            
             <Header
                 statusBarProps={{ barStyle: "light-content" }}
                 barStyle="light-content" // or directly
-                rightComponent={<Icon name="settings" color="white" onPress={() => displaySettingsScreen()}></Icon> }
+                // rightComponent={<Icon name="settings" color="white" onPress={() => displaySettingsScreen()}></Icon> }
+                rightComponent={<Icon name="settings" color="white" onPress={() => navigation.navigate('WorkoutGame')}></Icon> }
                 centerComponent={{
                 text: "Your Progression",
                 style: { color: "#FFF", fontSize: 22 }
