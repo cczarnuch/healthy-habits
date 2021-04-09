365275a87744ee08774341a0a932d41e77e311ba
TAsif <ta.asif100@gmail.com>
2021-04-09 16:44:17 -0400

    updatePoints throughout games

--- HealthyHabits/App.js
+++ HealthyHabits/App.js
@@ -12,30 +12,35 @@ import JournalGame from './src/components/Games/JournalGame';
 
 const App = () => {  
   const [display, setDisplay] = useState(true)
+  const [points, setPoints] = useState(0)
 
   const handleGame = (gameId) => {
     switch(gameId) {
       case 1:
-        setDisplay(<WorkoutGame />)
+        setDisplay(<WorkoutGame updatePoints={updatePoints} setMain={setDisplay} />)
         break
       case 2:
-        setDisplay(<MathGame />)
+        setDisplay(<MathGame updatePoints={updatePoints} />)
         break
       case 4:
-        setDisplay(<MemoryGame />)
+        setDisplay(<MemoryGame updatePoints={updatePoints} />)
         break
       case 6:
-        setDisplay(<JournalGame />)
+        setDisplay(<JournalGame updatePoints={updatePoints} />)
         break
       default:
         setDisplay(true)
     }
   }
 
+  const updatePoints = (number) => {
+    setPoints(points + number)
+  }
+
   return (
     display === true ? <Swiper loop={false} showsPagination={false} index={1}>
       <CalendarPage styles={styles} />
-      <MountainView styles={styles} />
+      <MountainView styles={styles} points={points} />
       <GameDisplay styles={styles} handleGame={handleGame} />
     </Swiper> : display
   );
