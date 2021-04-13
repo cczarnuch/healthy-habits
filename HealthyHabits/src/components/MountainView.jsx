5dfbf7802b9d6bd99102b5e7d010040d0f395bd2
Michelle Domagala-Tang <45515385+domagalm@users.noreply.github.com>
2021-04-13 00:25:12 -0400

    Updated score to screen logic

--- HealthyHabits/src/components/MountainView.jsx
+++ HealthyHabits/src/components/MountainView.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Image, Button, StyleSheet, Text, View } from 'react-native';
 import styles from '../styles/main.js';
 import m1 from '../assets/mountain1.png'; import m2 from '../assets/mountain2.png'; import m3 from '../assets/mountain3.png'; import m4 from '../assets/mountain4.png'; import m5 from '../assets/mountain5.png'; import m6 from '../assets/mountain6.png'; import m7 from '../assets/mountain7.png';
@@ -15,20 +15,31 @@ import Setting from '../components/SettingScreen';
 const MountainView = (props) => {
 
     const [visible, setVisible] = useState(false);
-    var score = 0;
-    var image = m1;
+    var [score, setScore] = useState(props.points);
+    var [image, setImage] = useState(m2);
 
     const displaySettingsScreen = () => {
         console.log("toggle pressed")
         setVisible(!visible);
     };
 
-    const switchMountainImg = () => {
-        console.log("switch mountain image")
+    useEffect(() => {
+        Image();
+      }, [props.points]);
+
+    function Image() {
+        if (props.points < 20) {
+            setImage(m2)
+        } 
+        else if (props.points >= 20 && props.points < 40) { setImage(m3) }
+        else if (props.points >=40 && props.points < 60) { setImage(m4) }
+        else if (props.points >= 60 && props.points < 80) { setImage(m5) }
+        else if (props.points >= 80 && props.points < 100) { setImage(m6) }
+        else { setImage(m7) }
     }
 
     return (
-        <SafeAreaProvider >
+        <SafeAreaProvider>
             <ImageBackground source={image} style={styles.img}>            
             <Header
                 statusBarProps={{ barStyle: "light-content" }}
@@ -55,7 +66,8 @@ const MountainView = (props) => {
                     barStyle="light-content" 
                     rightComponent={<Icon  name="close" color="white" size={30} onPress={displaySettingsScreen}></Icon>}
                     centerComponent={{text: "Settings",style: { color: "#FFF", fontSize: 22 }}}
-                    containerStyle={{backgroundColor: "#85CBCC"}}
+                    containerStyle={{backgroundColor: "#85CBCC",justifyContent: "space-around", 
+                    borderTopLeftRadius: 40, borderTopRightRadius: 40}}
                 />
                 <Setting 
                     updateSetting={props.updateSetting}
