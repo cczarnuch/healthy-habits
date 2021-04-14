834962fde02ecfd7f6251bcfc8c00eb0a96f9ed6
alkoasma <alkoasma@mcmaster.ca>
2021-04-13 21:25:59 -0400

    Added Calendar Markings

--- HealthyHabits/src/components/Calendar.jsx
+++ HealthyHabits/src/components/Calendar.jsx
@@ -22,8 +22,8 @@ const CalendarPage = (props) => {
   const [visible, setVisible] = useState(false);
 
   const displaySettingsScreen = () => {
-      console.log("toggle pressed")
-      setVisible(!visible);
+    console.log("toggle pressed")
+    setVisible(!visible);
   };
 
   return (
@@ -31,7 +31,7 @@ const CalendarPage = (props) => {
       <Header
         statusBarProps={{ barStyle: "light-content" }}
         barStyle="light-content" // or directly
-        rightComponent={<Icon name="settings" color="white" onPress={displaySettingsScreen}></Icon> }
+        rightComponent={<Icon name="settings" color="white" onPress={displaySettingsScreen}></Icon>}
         centerComponent={{
           text: "Calendar",
           style: { color: "#FFF", fontSize: 22 }
@@ -55,22 +55,27 @@ const CalendarPage = (props) => {
           scrollEnabled={true}
           // Enable or disable vertical scroll indicator. Default = false
           showScrollIndicator={true}
+          markedDates={{
+            '2021-04-09': { marked: true, dotColor: '#5DB9BB' },
+            '2021-04-10': { marked: true, dotColor: '#5DB9BB' },
+            '2021-04-14': { selected: true, marked: true, selectedColor: '#85CBCC', dotColor: 'white' }
+          }}
           onDayPress={day => {
-            let workout_log = "";   let math_log = "";   let word_log = "";
-            let memorization_log = "";   let meditation_log = "";   let journal_log = "";
+            let workout_log = ""; let math_log = ""; let word_log = "";
+            let memorization_log = ""; let meditation_log = ""; let journal_log = "";
             console.log(props.playerData);
             //console.log(props.playerData[day.dateString].math);
-            if (day.dateString in props.playerData) { 
+            if (day.dateString in props.playerData) {
               if ("workout" in props.playerData[day.dateString]) { workout_log = props.playerData[day.dateString].workout + " points"; }
               if ("math" in props.playerData[day.dateString]) { math_log = props.playerData[day.dateString].math + " points"; }
               if ("word" in props.playerData[day.dateString]) { word_log = props.playerData[day.dateString].word + " points"; }
               if ("memorization" in props.playerData[day.dateString]) { memorization_log = props.playerData[day.dateString].memorization + " points"; }
               if ("meditation" in props.playerData[day.dateString]) { meditation_log = props.playerData[day.dateString].meditation + " points"; }
-              if ("journal" in props.playerData[day.dateString]) { 
+              if ("journal" in props.playerData[day.dateString]) {
                 let temp = props.playerData[day.dateString].journal;
-                if (temp.includes(null)) { temp[0] = "";}; 
+                if (temp.includes(null)) { temp[0] = ""; };
                 journal_log = "Mood was a " + temp[1] + " out of 10. " + temp[0];
-              }      
+              }
             }
             else { console.log("Date selected does not exist in playerData"); }
 
@@ -83,12 +88,12 @@ const CalendarPage = (props) => {
               "Meditation Game:  " + meditation_log + "\n\n" +
               "Journal:  " + journal_log,
               [
-                  {
-                      text: "OK",
-                      onPress: () => console.log("Closed activity info screen")
-                  }
+                {
+                  text: "OK",
+                  onPress: () => console.log("Closed activity info screen")
+                }
               ]
-          )
+            )
           }}
           maxDate={new Date()}
           vertical={true}
@@ -98,18 +103,18 @@ const CalendarPage = (props) => {
       {/* Settings Overlay toggled when settings Icon is clicked */}
       <Overlay isVisible={visible} fullScreen={true} overlayStyle={[props.dark ? styles.darkContainer : styles.container]}>
         <Header
-            statusBarProps={{ barStyle: "light-content" }}
-            barStyle="light-content" 
-            rightComponent={<Icon  name="close" color="white" style={styles.back} size={30} onPress={displaySettingsScreen}></Icon>}
-            centerComponent={{text: "Settings",style: { color: "#FFF", fontSize: 22 }}}
-            containerStyle={{backgroundColor: "#85CBCC",justifyContent: "space-around"}}
+          statusBarProps={{ barStyle: "light-content" }}
+          barStyle="light-content"
+          rightComponent={<Icon name="close" color="white" style={styles.back} size={30} onPress={displaySettingsScreen}></Icon>}
+          centerComponent={{ text: "Settings", style: { color: "#FFF", fontSize: 22 } }}
+          containerStyle={{ backgroundColor: "#85CBCC", justifyContent: "space-around" }}
         />
-        <Setting 
+        <Setting
           updateSetting={props.updateSetting}
           dark={props.dark} diffculty={props.diffculty}>
         </Setting>
 
-    </Overlay>
+      </Overlay>
 
     </SafeAreaProvider>
   );
